c89365b98e126d23701af2f933cdf35473ff8abc
Farhan Tariq <drfarhantariq@outlook.com>
2026-03-28 20:48:36 +0500

    Add configuration class for bot settings
    Initialize configuration settings for the bot, including API credentials and database connection details.

---
+++ plugins/__init__.py
@@ -0,0 +1,18 @@
+from os import environ 
+
+class Config:
+    API_ID = environ.get("API_ID", "28361668")
+    API_HASH = environ.get("API_HASH", "50b661fce21cd08a19d36a9761263d8b")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7642568079:AAFDyoAztVulgEt0JHKdcOTsVi0vyJ9afj4") 
+    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Cluster0:Cluster0@cluster0.i0vsig5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6351480908').split()]
+
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
+    
